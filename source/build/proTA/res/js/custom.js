00969fed5c8a51d39757de9a3521beae2704c7f5
Manuel Vargas <manuelvargas@my.unt.edu>
2017-11-15 10:14:01 -0600

    searchbox retrieval

--- source/build/proTA/res/js/custom.js
+++ source/build/proTA/res/js/custom.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){ // document ready event occurs after the DOM has been loaded making it a great place to have your jQuery
 
-  $(".icon-plus").click(function() {
-    console.log ( $(".class-add" ).val() );
-  });
+	$(".icon-plus").click(function() {
+		console.log ( $(".class-add" ).val() );
+	});
+
+
+	//retrieve search text
+	$("#searchbox").keypress(function(e){
+
+		if(e.which == 13) {
+			let mytext = $(this).val();
+
+			console.log(mytext);
+		}
+	})
 
 });
