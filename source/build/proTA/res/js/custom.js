01ffbb368f639316df2dd566d8b4019c82ce2438
NicolasStencel <nicolas.stencel@gmail.com>
2017-11-14 23:59:56 -0600

    Add class when push add button

---
+++ source/build/proTA/res/js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){ // document ready event occurs after the DOM has been loaded making it a great place to have your jQuery
+
+  $(".icon-plus").click(function() {
+    console.log ( $(".class-add" ).val() );
+  });
+
+});
