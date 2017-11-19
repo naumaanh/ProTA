0a2aa0866d884c6b7f6b3e2d67eedfc3fb088a50
Manuel Vargas <manuelvargas@my.unt.edu>
2017-11-19 15:32:47 -0600

    course class

--- source/build/proTA/res/js/main.js
+++ source/build/proTA/res/js/main.js
@@ -1,59 +1,59 @@
 var pro = {
-  $view: 'null',
+	$view: 'null',
 
-  init: function(){
-    window.myApp = new Framework7();
-    window.$$ = Dom7;
-    pro.initF7();
-    pro.loadView();
-    pro.initSearchBar();
-  },
+	init: function(){
+		window.myApp = new Framework7();
+		window.$$ = Dom7;
+		pro.initF7();
+		pro.loadView();
+		pro.initSearchBar();
+	},
 
-  initF7: function(){
-    window.mainView = myApp.addView('.view-main', {
-      material: true,
-      cache: false
-    });
-  },
+	initF7: function(){
+		window.mainView = myApp.addView('.view-main', {
+			material: true,
+			cache: false
+		});
+	},
 
-  loadView: function(){
-    pro.setView();
-    pro.initEventHandlers();
-  },
+	loadView: function(){
+		pro.setView();
+		pro.initEventHandlers();
+	},
 
-  setView: function() {
+	setView: function() {
 		pro.$view = $( document.body ).find( '.view-main' );
 	},
 
-  initEventHandlers: function(){
-    pro.$view.on( 'click', '[pro-click]', function( e ){
-      $elem = $(this);
-      e.stopPropagation();
-      var toCall = $elem.attr( 'pro-click' ).replace( /\(.*?\)/, '' );
-      pro.methodCaller( toCall, $elem );
-    });
-    pro.$view.on( 'change', '[pro-change]', function( e ){
-      $elem = $(this);
-      e.stopPropagation();
-      var toCall = $elem.attr( 'pro-change' ).replace( /\(.*?\)/, '' );
-      pro.methodCaller( toCall, $elem );
-    });
-    $$(document).on('pageInit', function (e) {
-      var page = e.detail.page;
-      pro.refreshAutoInit(page);
-    });
-  },
+	initEventHandlers: function(){
+		pro.$view.on( 'click', '[pro-click]', function( e ){
+			$elem = $(this);
+			e.stopPropagation();
+			var toCall = $elem.attr( 'pro-click' ).replace( /\(.*?\)/, '' );
+			pro.methodCaller( toCall, $elem );
+		});
+		pro.$view.on( 'change', '[pro-change]', function( e ){
+			$elem = $(this);
+			e.stopPropagation();
+			var toCall = $elem.attr( 'pro-change' ).replace( /\(.*?\)/, '' );
+			pro.methodCaller( toCall, $elem );
+		});
+		$$(document).on('pageInit', function (e) {
+			var page = e.detail.page;
+			pro.refreshAutoInit(page);
+		});
+	},
 
-  refreshAutoInit: function( activeTab, prevElem ){
-			var autoInit = activeTab.find('[auto-init]');
-			autoInit = activeTab.attr('auto-init') ? autoInit.add( activeTab ) : autoInit;
-			autoInit.each( function(){
-				$elem = $(this);
-				pro.methodCaller( $elem.attr('auto-init').replace('@', ''), $elem, prevElem );
-			});
-		},
+	refreshAutoInit: function( activeTab, prevElem ){
+		var autoInit = activeTab.find('[auto-init]');
+		autoInit = activeTab.attr('auto-init') ? autoInit.add( activeTab ) : autoInit;
+		autoInit.each( function(){
+			$elem = $(this);
+			pro.methodCaller( $elem.attr('auto-init').replace('@', ''), $elem, prevElem );
+		});
+	},
 
-  getObj: function( objString, itter, obj ){
+	getObj: function( objString, itter, obj ){
 		var objs = objString.split('.');
 		var obj = obj ? obj[ objs[itter] ] : window[ objs[itter] ];
 		itter++;
@@ -65,7 +65,7 @@ var pro = {
 			return pro.methodExeption;
 	},
 
-  methodCaller: function( toCall, $elem, prevElem ){
+	methodCaller: function( toCall, $elem, prevElem ){
 		var func = pro.getObj( toCall, 0 );
 		if( func == pro.methodExeption )
 			prevElem = toCall;
@@ -76,61 +76,70 @@ var pro = {
 		console.log( 'Exeption unhandled function', $(this), funcName );
 	},
 
-	
-	
-	
-	
-  openPage: function( id ){
-    id = id ? id : this.attr('page-id');
-    mainView.router.loadContent( pro.$view.find(id).html() );
-  },
 
-  closePage: function(){
-    mainView.router.back();
-  },
 
-  openPopup: function( id ){
-    id = id ? id : this.attr('popup-id');
-    myApp.popup( pro.$view.find(id).html() );
-  },
 
-  initSearchBar: function(){
-    var mySearchbar = myApp.searchbar('.searchbar', {
-      searchList: '.list-block-search',
-      searchIn: '.item-title'
-    });
-  },
-	
-	
-	
-	
-	
-	//teacher class
-	teacher: {
-		
-		addClass: function(newClass){
-			this.classes.push(newClass);
-		},
-		
-		addGrades: function(){
-			console.log("grades added");
-		},
-		//,
-		//getClasses: function(){	
-		//}
+
+	openPage: function( id ){
+		id = id ? id : this.attr('page-id');
+		mainView.router.loadContent( pro.$view.find(id).html() );
 	},
-	
+
+	closePage: function(){
+		mainView.router.back();
+	},
+
+	openPopup: function( id ){
+		id = id ? id : this.attr('popup-id');
+		myApp.popup( pro.$view.find(id).html() );
+	},
+
+	initSearchBar: function(){
+		var mySearchbar = myApp.searchbar('.searchbar', {
+			searchList: '.list-block-search',
+			searchIn: '.item-title'
+		});
+	},
+
+
+
+
+	//course class
+	course: {
+		courses: [],
+		listItemTemplate: '<li></li>',
+
+		addCourse: function( div ){
+			var jsonObj = pro.toJSON( div );
+			pro.course.courses.push( jsonObj );
+			$('.page').data(pro.course.courses);
+		},
+
+		listCourses: function(){
+			var page = this;
+			var jsonObj = page.data();
+			$.each( jsonObj, function(){
+				page.append( pro.course.listItem( this ) );
+			});
+		},
+
+		listItem: function( $elem ){
+			return $(this.listItemTemplate).clone().text( $elem.courseName ).data( $elem.courseInstances );
+		},
+
+	},
+
 	//student class
 	student: {
-		
+
 		checkIn: function(date){
 			//push date into student checkin array	
 		}
-		
-		
-		
+
+
+
 	}
-	
-	
-	
+
+
+
 };
