66bc1d253c98aeada72ebcebaa803383c03fbbb2
Manuel Vargas <manuelvargas@my.unt.edu>
2017-11-26 19:09:55 -0600

    more tests

--- source/build/proTA/tests/addStudent.js
+++ source/build/proTA/tests/addStudent.js
@@ -3,10 +3,12 @@ function addStudent(student,id) {
 	
 	let course = [];
 	
+	let studentObj = JSON.parse(student + id)
+	
+	
 	course.push(courseName);
 	//pro.course.courses.push( jsonObj );
 	//return number of classes, should be equal to previous amount of classes plus 1?
-	
 	return tempclass;
 }
 module.exports = addStudent;
